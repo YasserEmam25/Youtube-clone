b3b54f4033d9bb83b9fffed72316b1bc468503c5
Yasser Emam <YasserEmam2002@gmail.com>
2022-07-11 02:48:09 +0200

    finished navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,44 @@
+import React, { useState } from "react";
 import "../css/App.css";
 import Header from "./Header";
+import SearchPage from "./SearchPage";
 import Sidebar from "./Sidebar";
 import RecommendedVideos from "./RecommendedVideos.js";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         {/* Headder <Header />*/}
-        {/* <Header /> */}
+        <Header />
 
-        <Switch>
-          <Route path="/">Home page</Route>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="app-page">
+                {/* sidebar <Sidebar /> */}
+                <Sidebar />
+                {/* Recommended videos  */}
+                <RecommendedVideos />
+              </div>
+            }
+          />
 
-          <Route path="/search">search</Route>
-        </Switch>
+          <Route
+            path="/search"
+            element={
+              <div className="app-page">
+                {/* sidebar <Sidebar /> */}
+                <Sidebar />
+                {/* search results  */}
+                <SearchPage />
+              </div>
+            }
+          />
+        </Routes>
       </Router>
-
-      <div className="app-page">
-        {/* sidebar <Sidebar /> */}
-        {/* <Sidebar /> */}
-        {/* Recommended videos  */}
-        {/* <RecommendedVideos /> */}
-      </div>
     </div>
   );
 }
